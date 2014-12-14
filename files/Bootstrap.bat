6d013d48959253c449a92708a985757c28e48149
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2014-12-14 12:17:49 +0100

    Initialization of the Puppet module

---
+++ files/Bootstrap.bat
@@ -0,0 +1,46 @@
+rem cmd
+
+rem Set parameters
+set PuppetPath=C:\ProgramData\PuppetLabs\puppet\etc
+
+rem Install Chocolatey, get this script from https://chocolatey.org/
+call cmd /c @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+
+rem Install packages
+call cinst poshgit
+call cinst puppet
+call cinst curl
+call cinst wget
+
+rem Set Puppet and Ruby path, get this from Start Command Prompt with Puppet, echo %path% > path.txt
+call SET PATH=%PATH%;C:\Program Files\Puppet Labs\Puppet\puppet\bin;C:\Program Files\Puppet Labs\Puppet\facter\bin;C:\Program Files\Puppet Labs\Puppet\hiera\bin;C:\Program Files\Puppet Labs\Puppet\bin;C:\Program Files\Puppet Labs\Puppet\sys\ruby\bin;C:\Program Files\Puppet Labs\Puppet\sys\tools\bin;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;C:\ProgramData\chocolatey\bin;C:\Program Files (x86)\Git\cmd;C:\Program Files\Puppet Labs\Puppet\bin 
+
+rem Create custom fact to set data source (hiera yaml file)
+call puppet apply -e "file { 'c:/ProgramData/PuppetLabs/facter/facts.d/data_source.txt': content => 'data_source=%1' }"
+
+rem Install GeoTrust certificate to download modules from Puppetforge, see https://docs.puppetlabs.com/windows/troubleshooting.html#error-messages
+call wget --no-check-certificate https://www.geotrust.com/resources/root_certificates/certificates/GeoTrust_Global_CA.pem
+call certutil -addstore Root GeoTrust_Global_CA.pem
+
+rem Install AddTrust certificate, see https://gist.github.com/luislavena/f064211759ee0f806c88
+call wget --no-check-certificate https://raw.githubusercontent.com/rubygems/rubygems/master/lib/rubygems/ssl_certs/AddTrustExternalCARoot-2048.pem
+call copy AddTrustExternalCARoot-2048.pem "C:\Program Files\Puppet Labs\Puppet\sys\ruby\lib\ruby\2.0.0\rubygems\ssl_certs"
+
+rem Install r10k
+call gem.bat install r10k
+
+rem Download Puppetfile
+call curl -k https://raw.githubusercontent.com/naturalis/puppet-windows_role_base/master/files/Puppetfile > %PuppetPath%\Puppetfile
+
+rem Run r10k to download modules
+cd %PuppetPath%
+call r10k --verbose 3 puppetfile install
+
+rem Copy hiera.yaml (hiera config file)
+call copy %PuppetPath%\modules\windows_base\files\hiera.yaml C:\ProgramData\PuppetLabs\puppet\etc
+
+rem Copy yaml files (userdata)
+call copy %PuppetPath%\modules\windows_role_base\files\*.yaml C:\ProgramData\PuppetLabs\hiera\var
+
+rem Apply base module
+puppet apply -e 'include windows_role_base'
---
+++ manifests/init.pp
@@ -0,0 +1,86 @@
+# == Class: windows_role_base
+#
+# Full description of class windows_role_base here.
+#
+# === Parameters
+#
+# Document parameters here.
+#
+# [*sample_parameter*]
+#   Explanation of what this parameter affects and what it defaults to.
+#   e.g. "Specify one or more upstream ntp servers as an array."
+#
+# === Variables
+#
+# Here you should define a list of variables that this module would require.
+#
+# [*sample_variable*]
+#   Explanation of how this variable affects the funtion of this class and if
+#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
+#   External Node Classifier as a comma separated list of hostnames." (Note,
+#   global variables should be avoided in favor of class parameters as
+#   of Puppet 2.6.)
+#
+# === Examples
+#
+#  class { windows_role_base:
+#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
+#  }
+#
+# === Authors
+#
+# Author Name <author@domain.com>
+#
+# === Copyright
+#
+# Copyright 2014 Your name here, unless otherwise noted.
+#
+class windows_role_base (
+  $packages_attribute_defaults = { ensure   => installed,
+                                   provider => chocolatey,
+                                 },
+
+  $packages_hash = { 'puppet' => { ensure   => latest,
+                                   provider => chocolatey,
+                                 },
+
+                     'git'    => { ensure   => latest,
+                                   provider => chocolatey, 
+                                 },
+
+                   },
+              
+  $users_attribute_defaults = { ensure => present,
+                              },
+                             
+  $users_hash = { 'rudi.broekhuizen' => { comment  => 'Rudi Broekhuizen',
+                                          password => 'Passw0rd',
+                                          #email   => 'rudi.broekhuizen@naturalis.nl',
+                                          groups   => ['Administrators'],
+                                        }
+                },
+  ) {
+
+# General configuration
+  class {'windows_base::config':
+  }
+
+# Install files
+  class {'windows_base::files':
+    require => Class["windows_base::config"],
+  }
+
+# Install packages
+  class {'windows_base::packages':
+    packages_hash               => $packages_hash,
+    packages_attribute_defaults => $packages_attribute_defaults,
+    require                     => Class["windows_base::files"],
+  }
+
+# Install users
+  class {'windows_base::users':
+    users_hash               => $users_hash,
+    users_attribute_defaults => $users_attribute_defaults,
+    require                  => Class["windows_base::packages"],
+  }
+}
