51a774e968455a57ffe7be419113c65554386439
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-09-28 13:37:07 +0200

    Update Bootstrap.bat

--- files/Bootstrap.bat
+++ files/Bootstrap.bat
@@ -46,3 +46,6 @@ call copy %PuppetPath%\modules\windows_role_base\files\*.yaml C:\ProgramData\Pup
 
 rem Apply base module
 puppet apply -e 'include windows_role_base'
+
+rem Apply additional modules
+puppet apply -e 'hiera_include('classes')'
